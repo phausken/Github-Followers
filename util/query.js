be3bbce0edb5380601188c5094bdbd1753c9fb10
Peter Hausken <phausken@gmail.com>
2018-05-16 21:45:37 -0400

    Fix CSS for search buttons. Prettify JS files

--- util/query.js
+++ util/query.js
@@ -1,47 +1,45 @@
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 
-
-
-export const fetchFollowers =  gql`
-  query FindFollowers($username: String!){
-  user(login: $username){
-		id,
-    login,
-    bio,
-    name,
-    avatarUrl,
-    followers(first: 20){
-      totalCount
-      nodes{
-        avatarUrl
-        login
-        id
-      }
-      pageInfo{
-        endCursor
-        hasNextPage
+export const fetchFollowers = gql`
+  query FindFollowers($username: String!) {
+    user(login: $username) {
+      id
+      login
+      bio
+      name
+      avatarUrl
+      followers(first: 20) {
+        totalCount
+        nodes {
+          avatarUrl
+          login
+          id
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
       }
     }
   }
-}
 `;
 
-export const fetchMoreFollowers =  gql`
-  query FindFollowers($username: String!, $after: String!){
-  user(login: $username){
-    id,
-    followers(first: 20, after: $after){
-      totalCount
-      nodes{
-        avatarUrl
-        login
-        id
-      }
-      pageInfo{
-        endCursor
-        hasNextPage
+export const fetchMoreFollowers = gql`
+  query FindFollowers($username: String!, $after: String!) {
+    user(login: $username) {
+      id
+      followers(first: 20, after: $after) {
+        totalCount
+        nodes {
+          avatarUrl
+          login
+          id
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
       }
     }
   }
-}`
-;
+`;
